be920662af03693ae8db8e04435f6694360713dd
Sherena Tu <s.tu314@yahoo.com>
2021-08-19 22:26:36 -0700

    Intial Commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+//
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
+
+app.get("/weather/:temperature", (req, res) => {
+  const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+
+  res.status(200).send(phrase);
+});
+
+app.get("/api/users", (req, res) => {
+  let stuff = ["itemA", "itemB", "itemC", "itemD"];
+  res.status(200).send(stuff);
+});
+
+app.listen(4000, () => console.log("Server running on 4000"));
